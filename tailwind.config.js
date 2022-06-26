a9ec9e28fb2a03251b3b644c93b86e2a03f0a6da
opatricpires <patric.pires@outlook.com>
2022-06-26 18:44:09 -0300

    feat: create project

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      primary: {
+        "dark-cyan": "hsl(158, 36%, 37%)",
+        cream: "hsl(30, 38%, 92%)",
+      },
+      neutral: {
+        "very-dark-blue": "hsl(212, 21%, 14%)",
+        "dark-grayish-blue": "hsl(228, 12%, 48%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+    },
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"],
+      serif: ["Fraunces", "serif"],
+    },
+    extend: {},
+  },
+
+  plugins: [],
+};
